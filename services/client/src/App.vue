bc05e6aba9bd8a98bccb491ff9912d520a867962
1Mill <erik.ekberg@outlook.com>
2020-06-13 13:56:00 -0700

    Update App.vue

--- services/client/src/App.vue
+++ services/client/src/App.vue
@@ -2,7 +2,21 @@
 export default {
 	methods: {
 		produce({ topic }) {
-			console.log(topic);
+			const url = `${process.env.VUE_APP_RAPIDS_PROXY_URL}/topics/${topic}`;
+			fetch(url, {
+				body: JSON.stringify({
+					records: [
+						{ value: "testing" }
+					]
+				}),
+				headers: {
+					'Accept': 'application/vnd.kafka.v2+json',
+					'Content-Type': 'application/vnd.kafka.json.v2+json',
+				},
+				method: 'POST',
+			})
+			.then(res => res.json())
+			.then(data => console.log(data));
 		},
 	},
 };
