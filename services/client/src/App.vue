1f6b7d88f17507dac70ee9843ba40b28c560f7be
1Mill <erik.ekberg@outlook.com>
2020-06-13 11:20:43 -0700

    Scaffold method to produce topic

--- services/client/src/App.vue
+++ services/client/src/App.vue
@@ -1,5 +1,18 @@
+<script>
+export default {
+	methods: {
+		produce({ topic }) {
+			console.log(topic);
+		},
+	},
+};
+</script>
+
 <template>
 	<main>
 		<h1>Hello world!</h1>
+		<button @click='produce({ topic: "hello-world" })'>
+			Create "hello-world" topic
+		</button>
 	</main>
 </template>
