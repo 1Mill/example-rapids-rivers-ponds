7f20197c14587dfe5a69beea9dbf8b4600766df7
1Mill <erik.ekberg@outlook.com>
2020-06-13 14:26:14 -0700

    Wrap produce in try/catch

--- services/client/src/utilities/rapids/produce.js
+++ services/client/src/utilities/rapids/produce.js
@@ -1,17 +1,21 @@
 export const produce = ({ topic }) => {
-	const { href } = new URL(`/topics/${topic}`, process.env.VUE_APP_RAPIDS_PROXY_URL);
-	fetch(href, {
-		body: JSON.stringify({
-			records: [
-				{}
-			]
-		}),
-		headers: {
-			'Accept': 'application/vnd.kafka.v2+json',
-			'Content-Type': 'application/vnd.kafka.json.v2+json',
-		},
-		method: 'POST',
-	})
-	.then(res => res.json())
-	.then(data => console.log(data));
+	try {
+		const { href } = new URL(`/topics/${topic}`, process.env.VUE_APP_RAPIDS_PROXY_URL);
+		fetch(href, {
+			body: JSON.stringify({
+				records: [
+					{}
+				]
+			}),
+			headers: {
+				'Accept': 'application/vnd.kafka.v2+json',
+				'Content-Type': 'application/vnd.kafka.json.v2+json',
+			},
+			method: 'POST',
+		})
+		.then(res => res.json())
+		.then(data => console.log(data));
+	} catch(err) {
+		console.error(err);
+	}
 };
