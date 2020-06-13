e6fd901941af660cf8a2397eefc7b65f406f0465
1Mill <erik.ekberg@outlook.com>
2020-06-13 09:35:42 -0700

    Connect hello-world service to rapids

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!")
+const { WATERWAY_TYPE_KAFKA, waterway } = require("@1mill/waterway");
+
+const ID = 'hello-world-service';
+const TOPICS = ['hello-world'];
+
+const main = async() => {
+	try {
+		waterway({
+			id: ID,
+			type: WATERWAY_TYPE_KAFKA,
+			url: process.env.RAPIDS_URL,
+		}).subscribe({
+			onEvent: ({ event }) => {
+				console.log('Hello world!');
+				// console.log(event);
+			},
+			topics: TOPICS,
+		})
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+main();
