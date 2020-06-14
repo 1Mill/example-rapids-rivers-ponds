60b9a6e8b1dc9ac595e1fc77080276f989cb5416
1Mill <erik.ekberg@outlook.com>
2020-06-14 15:27:57 -0700

    Outline process

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -1,11 +1,18 @@
-const { getData } = require('./utilities/cloudevents/getData')
+const { getData } = require('./utilities/cloudevents/getData');
 const { subscribe } = require('./utilities/subscribe');
 
 subscribe({
 	handler: ({ cloudevent }) => {
+		// Parse data
 		const data = getData({ cloudevent });
-		if (data === '') { console.log('THIS THING IS EMPTY!') }
+
+		// Buisness / Domain logic
+		if (data === '') { return; }
 		console.log(data);
+
+		// Enrich event
+
+		// Publish enriched event to rapids
 	},
 	topic: 'hello-world',
 });
