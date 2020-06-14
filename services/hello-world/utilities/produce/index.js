6b3a58eea0afaaa0e94fccce4e77a3f807319667
1Mill <erik.ekberg@outlook.com>
2020-06-14 14:48:20 -0700

    Copy + paste produce and subscribe from client services

---
+++ services/hello-world/utilities/produce/index.js
@@ -0,0 +1,27 @@
+const { Kafka } = require('kafkajs');
+
+const CLIENT_ID = 'hello-world-service';
+const KAFKA = new Kafka({
+	brokers: [process.env.RAPIDS_URL],
+	clientId: CLIENT_ID,
+});
+
+export const publish = async ({ id, payloads=[ '' ], topic }) => {
+	const { connect, disconnect, send } = KAFKA.producer();
+	await connect();
+	await send({
+		messages: payloads.map((payload) => ({
+			headers: { contentType: 'application/cloudevents+json;charset=UTF-8' },
+			// https://github.com/cloudevents/spec/blob/master/kafka-protocol-binding.md#334-example
+			value: JSON.stringify({
+				data: JSON.stringify(payload),
+				datacontenttype: 'application/json',
+				id,
+				specversion: '1.0',
+				time: new Date().toISOString(),
+			}),
+		})),
+		topic,
+	});
+	await disconnect();
+};
---
+++ services/hello-world/utilities/subscribe/index.js
@@ -0,0 +1,42 @@
+const { ERROR_TYPES, SIGNAL_TRAPS } = require('../../lib/constants');
+const { Kafka } = require('kafkajs');
+
+const CLIENT_ID = 'hello-world-service';
+const KAFKA = new Kafka({
+	brokers: [process.env.RAPIDS_URL],
+	clientId: CLIENT_ID,
+});
+
+export const subscribe = async({ id, topic }) => {
+	const { connect, disconnect, run, subscribe } = KAFKA.consumer({ groupId: CLIENT_ID });
+	await connect();
+	await subscribe({ topic, fromBeginning: true });
+	await run({
+		eachMessage: async({ topic, partition, message }) => {
+			console.log('Message received');
+		},
+	});
+	run().catch(err => console.error(err));
+
+	ERROR_TYPES.map((type) => {
+		process.on(type, async (e) => {
+			try {
+				console.log(`process.on ${type}`);
+				console.error(e);
+				await disconnect();
+				process.exit(0);
+			} catch (_) {
+				process.exit(1);
+			}
+		});
+	});
+	SIGNAL_TRAPS.map((type) => {
+		process.once(type, async () => {
+			try {
+				await disconnect();
+			} finally {
+				process.kill(process.pid, type);
+			}
+		});
+	});
+};
