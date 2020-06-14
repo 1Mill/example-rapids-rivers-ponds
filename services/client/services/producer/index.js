1d736d86e3f010d8041a0d62c597aa51b516fb53
1Mill <erik.ekberg@outlook.com>
2020-06-14 11:10:43 -0700

    Send multiple payloads

--- services/client/services/producer/index.js
+++ services/client/services/producer/index.js
@@ -3,7 +3,11 @@ const { publish } = require('./utilities/publish');
 console.log('Publishing');
 publish({
 	id: 'my-uuid-back-to-user-who-made-the-request',
-	payloads: [ 'testing' ],
+	payloads: [
+		'testing-1',
+		'testing-2',
+		'testing-3',
+	],
 	topic: 'hello-world',
 });
 console.log('Finished');
