470e932eb3d27520ae6995b53891809023f248f1
1Mill <erik.ekberg@outlook.com>
2020-06-14 15:25:07 -0700

    Rename function to getData

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -1,9 +1,9 @@
-const { applicationData } = require('./utilities/cloudevents/applicationData')
+const { getData } = require('./utilities/cloudevents/getData')
 const { subscribe } = require('./utilities/subscribe');
 
 subscribe({
 	handler: ({ cloudevent }) => {
-		const data = applicationData({ cloudevent });
+		const data = getData({ cloudevent });
 		if (data === '') { console.log('THIS THING IS EMPTY!') }
 		console.log(data);
 	},
