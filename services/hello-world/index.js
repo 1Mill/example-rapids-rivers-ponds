c1a55e5239074221e1113ceba2d0f98a7e5c5e9c
1Mill <erik.ekberg@outlook.com>
2020-06-14 19:20:57 -0700

    Output cloudevent data to console

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -5,7 +5,14 @@ const { subscribe } = require('./utilities/subscribe');
 subscribe({
 	brokers: [ process.env.RAPIDS_URL ],
 	eventType: 'kafka',
-	handler: ({ cloudevent }) => { console.log('Something happened'); },
+	handler: ({ cloudevent }) => {
+		console.log(cloudevent.id);
+		console.log(cloudevent.source);
+		console.log(cloudevent.time);
+		console.log(cloudevent.type);
+		console.log(cloudevent.data);
+		console.log(JSON.parse(cloudevent.data));
+	 },
 	id: 'hello-world-service',
 	type: 'hello-world-2020-06-14',
 });
