7739f52768aab14281ff720a4f36361ca29757ff
1Mill <erik.ekberg@outlook.com>
2020-06-14 19:38:48 -0700

    Scaffold future steps

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -5,18 +5,17 @@ subscribe({
 	brokers: [ process.env.RAPIDS_URL ],
 	eventType: 'kafka',
 	handler: ({ cloudevent }) => {
+		// Fetch application data
+
+		// Perform buisness / domain logic
+
+		// Enrich event
 		const enrichedCloudevent = enrich({
 			cloudevent,
 			enrichment: 'testing',
 		});
-		console.log(enrichedCloudevent.id);
-		// console.log(enrichedCloudevent.source);
-		// console.log(enrichedCloudevent.time);
-		// console.log(enrichedCloudevent.type);
-		// console.log(enrichedCloudevent.data);
-		// console.log(JSON.parse(enrichedCloudevent.data));
-		console.log(enrichedCloudevent.enrichment);
-		console.log(JSON.parse(enrichedCloudevent.enrichment));
+
+		// Publish enriched event
 	 },
 	id: 'hello-world-service',
 	type: 'hello-world-2020-06-14',
