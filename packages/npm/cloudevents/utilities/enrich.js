63d93b9880d0d29f68e5ee000a3d7c0b8e45c83b
1Mill <erik.ekberg@outlook.com>
2020-06-16 16:15:19 -0700

    Update enrich.js

---
+++ packages/npm/cloudevents/utilities/enrich.js
@@ -0,0 +1,14 @@
+const enrich = ({
+	cloudevent,
+	enrichment,
+	enrichmentcontenttype = "application/json",
+}) => {
+	return {
+		...cloudevent,
+		enrichment: JSON.stringify(enrichment),
+		enrichmentcontenttype,
+		time: new Date().toISOString(),
+	};
+};
+
+module.exports = { enrich };
