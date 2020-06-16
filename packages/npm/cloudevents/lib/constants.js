15a8d7ff2cb6606ce7afc0e4c920ae0da339df9d
1Mill <erik.ekberg@outlook.com>
2020-06-16 16:24:30 -0700

    Add more consts + export consts

--- packages/npm/cloudevents/lib/constants.js
+++ packages/npm/cloudevents/lib/constants.js
@@ -1,1 +1,9 @@
-const CLOUDEVENT_KAFKA_TYPE = 'kafka';
+const ERROR_TYPES = ["unhandledRejection", "uncaughtException"];
+const KAFKA_EVENTTYPE = "kafka";
+const SIGNAL_TRAPS = ["SIGTERM", "SIGINT", "SIGUSR2"];
+
+module.exports = {
+	ERROR_TYPES,
+	KAFKA_EVENTTYPE,
+	SIGNAL_TRAPS,
+};
