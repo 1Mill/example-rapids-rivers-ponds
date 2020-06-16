48048d295cc1fe27f9ef1c826b4413d4254c91a7
1Mill <erik.ekberg@outlook.com>
2020-06-15 17:12:39 -0700

    Install packages to support socket.io-redis

--- services/client/services/subscriber/index.js
+++ services/client/services/subscriber/index.js
@@ -1,6 +1,17 @@
+const ioRedisAdapter = require('socket.io-redis');
 const { KAFKA_EVENT_TYPE } = require('./lib/constants');
 const { isEnriched } = require('./utilities/cloudevents/isEnriched');
 const { subscribe } = require('./utilities/subscribe');
+
+const server = require('http').createServer();
+
+const io = require('socket.io')(server);
+io.adapter(
+	ioRedisAdapter({
+		host: process.env.REDIS_HOST,
+		port: process.env.REDIS_PORT,
+	})
+);
 
 subscribe({
 	brokers: [process.env.RAPIDS_URL],
@@ -14,11 +25,16 @@ subscribe({
 		// TODO: Abstract data parsing into framework;
 		const data = JSON.parse(cloudevent.data);
 		const enrichment = JSON.parse(cloudevent.enrichment);
+		const { id } = cloudevent;
 
 		// Perform buisness / domain logic
-		console.log(data);
-		console.log(enrichment);
+		console.log(id);
+
 	},
 	id: 'client-subscriber-service',
 	type: 'hello-world-2020-06-14',
+});
+
+server.listen(process.env.PORT, () => {
+	console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`);
 });
