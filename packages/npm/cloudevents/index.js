1ec1acdefcea22c210dfa658def889b01e351e6b
1Mill <erik.ekberg@outlook.com>
2020-06-16 16:14:39 -0700

    Update index.js

--- packages/npm/cloudevents/index.js
+++ packages/npm/cloudevents/index.js
@@ -1,5 +1,13 @@
 const { KAFKA_EVENTTYPE } = require('./lib/constants');
+const { create } = require('./utilities/create');
+const { enrich } = require('./utilities/enrich');
+const { isEnriched } = require('./utilities/isEnriched');
+const { toEventType } = require('./utilities/toEventType');
 
 module.exports = Object.freeze({
 	KAFKA_EVENTTYPE,
+	create,
+	enrich,
+	isEnriched,
+	toEventType,
 });
