f5fffd22414b7b8e4dbe9e1a0c757fb4db37a0fe
1Mill <erik.ekberg@outlook.com>
2020-06-17 12:42:10 -0700

    By defulat send 1 payload with value of null

--- services/client/client/src/utilities/cloudevent.js
+++ services/client/client/src/utilities/cloudevent.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 const SOCKET = io(process.env.VUE_APP_CLIENT_H_URL);
 
-export const publish = ({ type, payloads }) => {
+export const publish = ({ type, payloads=[ null ] }) => {
 	SOCKET.emit({ type, payloads });
 };
 
