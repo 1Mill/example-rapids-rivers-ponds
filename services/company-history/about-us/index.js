2adbac2f75991bb1db1b313c506988669e4b6c26
1Mill <erik.ekberg@outlook.com>
2020-06-16 21:02:48 -0700

    Scaffold company-history service

---
+++ services/company-history/about-us/index.js
@@ -0,0 +1,18 @@
+const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
+
+const ID = 'company-history-about-us-service';
+
+subscribe({
+	// eventType: KAFKA_EVENTTYPE,
+	// handler: async ({ cloudevent, data, enrichment, isEnriched }) => {
+	// 	if (isEnriched) { return; }
+
+	// 	const enrichmentValue = data.split('').reverse().join('')
+	// 	console.log(`Turn "${data}" into "${enrichmentValue}"`);
+	// 	return enrichmentValue;
+	// },
+	// id: ID,
+	// publishTo: [ process.env.RAPIDS_URL ],
+	// subscribeTo: [ process.env.RAPIDS_URL ],
+	// type: 'hello-world-2020-06-14',
+});
