50b4dfad5243324c12e2f3c3aed5e406ac31bf46
1Mill <erik.ekberg@outlook.com>
2020-06-16 20:00:40 -0700

    Update to use new cloudevents subscriber interface

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -3,12 +3,14 @@ const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
 const ID = 'hello-world-service';
 
 subscribe({
-	brokers: [ process.env.RAPIDS_URL ],
 	eventType: KAFKA_EVENTTYPE,
 	handler: async ({ data, enrichment, isEnriched }) => {
 		if (isEnriched) { return; }
+		console.log(data);
 		return data.split('').reverse().join('');
 	},
 	id: ID,
+	publishTo: [ process.env.RAPIDS_URL ],
+	subscribeTo: [ process.env.RAPIDS_URL ],
 	type: 'hello-world-2020-06-14',
 });
