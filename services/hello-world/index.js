98be286910225d9889e4a101d6debfee2fd4a1c8
1Mill <erik.ekberg@outlook.com>
2020-06-16 19:45:16 -0700

    Reduce handler to only buisness logic

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -1,52 +1,14 @@
-const {
-	KAFKA_EVENTTYPE,
-	enrich,
-	isEnriched,
-	publish,
-	subscribe,
-	toEventType,
-} = require('@1mill/cloudevents');
+const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
 
 const ID = 'hello-world-service';
 
 subscribe({
 	brokers: [ process.env.RAPIDS_URL ],
 	eventType: KAFKA_EVENTTYPE,
-	handler: async ({ cloudevent }) => {
-		// Escape clauses
-		// TODO: Abstract enrichment check into framework
-		if (isEnriched({ cloudevent })) { return; }
-
-		// Fetch buisness data
-		// TODO: Abstract data parsing into framework;
-		const data = JSON.parse(cloudevent.data);
-
-		// Perform buisness / domain logic
-		console.log(data);
-		const enrichment = data.split('').reverse().join('');
-		console.log(enrichment);
-
-		// Enrich event
-		// TODO: Abstract enrichment work into framework with
-		// TODO: value returned from buisness / domain logic
-		const enrichedCloudevent = enrich({
-			cloudevent,
-			enrichment,
-		});
-
-		// Publish enriched event
-		// TODO: Abstract publishing work into framework
-		await publish({
-			brokers: [ process.env.RAPIDS_URL ],
-			event: toEventType({
-				cloudevent: enrichedCloudevent,
-				eventType: KAFKA_EVENTTYPE,
-			}),
-			eventType: KAFKA_EVENTTYPE,
-			id: ID,
-		});
-	 },
+	handler: async ({ data, enrichment, isEnriched }) => {
+		if (isEnriched) { return; }
+		return data.split('').reverse().join('');
+	},
 	id: ID,
 	type: 'hello-world-2020-06-14',
 });
-
