69ac7b448b186d2543d986a31bc22c82d69afcee
1Mill <erik.ekberg@outlook.com>
2020-06-19 10:04:39 -0700

    Connect to database sservice

--- services/company-employes/index/index.js
+++ services/company-employes/index/index.js
@@ -1,25 +1,34 @@
+const assert = require('assert');
 const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
+const { MongoClient } = require('mongodb');
 
 const ID = 'company-employes-index-service';
 
-subscribe({
-	handler: async ({ isEnriched }) => {
-		if (isEnriched) {
-			return;
-		}
-		return [
-			{
-				description: 'I do the stuff with the things',
-				id: '1234',
-				name: 'Donald Smith',
-				title: 'Software engineer',
-			},
-		];
-	},
-	id: ID,
-	publishEventType: KAFKA_EVENTTYPE,
-	publishTo: [process.env.RAPIDS_URL],
-	subscribeEventType: KAFKA_EVENTTYPE,
-	subscribeTo: [process.env.RAPIDS_URL],
-	types: ['company-employes.index.2020-06-18'],
+const client = new MongoClient(process.env.DATABASE_URL)
+client.connect((err) => {
+	assert.equal(null, err)
+	console.log('Connected successfully to database')
+	const db = client.db(process.env.DATABASE_NAME)
+
+	subscribe({
+		handler: async ({ isEnriched }) => {
+			if (isEnriched) {
+				return;
+			}
+			return [
+				{
+					description: 'I do the stuff with the things',
+					id: '1234',
+					name: 'Donald Smith',
+					title: 'Software engineer',
+				},
+			];
+		},
+		id: ID,
+		publishEventType: KAFKA_EVENTTYPE,
+		publishTo: [process.env.RAPIDS_URL],
+		subscribeEventType: KAFKA_EVENTTYPE,
+		subscribeTo: [process.env.RAPIDS_URL],
+		types: ['company-employes.index.2020-06-18'],
+	});
 });
