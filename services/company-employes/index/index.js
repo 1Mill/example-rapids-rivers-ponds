6409ba986f3713f67d6c9f43ed2a8ad1cd2b1d2f
1Mill <erik.ekberg@outlook.com>
2020-06-18 22:20:11 -0700

    Update cloudevents package + interface and install mongo package

--- services/company-employes/index/index.js
+++ services/company-employes/index/index.js
@@ -3,9 +3,10 @@ const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
 const ID = 'company-employes-index-service';
 
 subscribe({
-	eventType: KAFKA_EVENTTYPE,
 	handler: async ({ isEnriched }) => {
-		if (isEnriched) { return; }
+		if (isEnriched) {
+			return;
+		}
 		return [
 			{
 				description: 'I do the stuff with the things',
@@ -13,10 +14,12 @@ subscribe({
 				name: 'Donald Smith',
 				title: 'Software engineer',
 			},
-		]
+		];
 	},
 	id: ID,
-	publishTo: [ process.env.RAPIDS_URL ],
-	subscribeTo: [ process.env.RAPIDS_URL ],
-	types: [ 'company-employes.index.2020-06-18' ],
+	publishEventType: KAFKA_EVENTTYPE,
+	publishTo: [process.env.RAPIDS_URL],
+	subscribeEventType: KAFKA_EVENTTYPE,
+	subscribeTo: [process.env.RAPIDS_URL],
+	types: ['company-employes.index.2020-06-18'],
 });
