9e2313f02242e31746221682bae3239b98b486f8
1Mill <erik.ekberg@outlook.com>
2020-06-19 13:34:47 -0700

    Fetch data from databaser service

--- services/company-employes/index/index.js
+++ services/company-employes/index/index.js
@@ -4,34 +4,55 @@ const { MongoClient } = require('mongodb');
 
 const ID = 'company-employes-index-service';
 
-const client = new MongoClient(process.env.DATABASE_URL)
-client.connect((err) => {
-	assert.equal(null, err)
-	console.log('Connected successfully to database')
-	const db = client.db(process.env.DATABASE_NAME)
+const client = new MongoClient(process.env.DATABASE_URL);
+const main = async () => {
+	try {
+		await client.connect();
+		const db = client.db(process.env.DATABASE_NAME);
+		const collection = db.collection('employees');
 
-	subscribe({
-		handler: async ({ isEnriched }) => {
-			if (isEnriched) {
-				return;
-			}
-			return [
-				{
-					description: 'I do the stuff with the things',
-					id: '1234',
-					name: 'Donald Smith',
-					title: 'Software engineer',
-				},
-			];
-		},
-		id: ID,
-		publishEventType: KAFKA_EVENTTYPE,
-		publishTo: [process.env.RAPIDS_URL],
-		subscribeEventType: KAFKA_EVENTTYPE,
-		subscribeTo: [process.env.RAPIDS_URL],
-		types: ['company-employes.index.2020-06-18'],
-	});
-});
+		// Remove all records from database
+		collection.deleteMany({});
+
+		// Seed database
+		const records = [
+			{
+				description: 'I do the stuff with the things',
+				id: '1234',
+				name: 'John Doe',
+				title: 'Software engineer',
+			},
+			{
+				description: 'I do the stuff with the things',
+				id: '1233',
+				name: 'Jane Doe',
+				title: 'Software engineer',
+			},
+		];
+		await collection.insertMany(records);
+
+		subscribe({
+			handler: async ({ isEnriched }) => {
+				if (isEnriched) { return; }
+
+				const results = collection
+					.find({ title: 'Software engineer' })
+					.toArray();
+				return results;
+			},
+			id: ID,
+			publishEventType: KAFKA_EVENTTYPE,
+			publishTo: [process.env.RAPIDS_URL],
+			subscribeEventType: KAFKA_EVENTTYPE,
+			subscribeTo: [process.env.RAPIDS_URL],
+			types: ['company-employes.index.2020-06-18'],
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+main();
 
 process.on('SIGINT', () => {
 	client.close();
