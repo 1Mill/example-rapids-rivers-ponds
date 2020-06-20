1d8d6edf8e91b26dcd809602af5360a13662b863
1Mill <erik.ekberg@outlook.com>
2020-06-19 19:51:25 -0700

    Create tab on create

--- services/client/client/src/App.vue
+++ services/client/client/src/App.vue
@@ -1,7 +1,15 @@
 <script>
+import { TabAggregate } from './utilities/TabAggregate';
 // import { publish, subscribe } from './utilities/cloudevent';
 
 export default {
+	created() {
+		const tab = new TabAggregate({
+			tableNumber: '12',
+			waiter: 'William Tell',
+		});
+		console.log(tab.id);
+	},
 };
 </script>
 
