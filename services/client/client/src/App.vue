8ff04bb6e20159b2e903e8828fd928a25f60e5a3
1Mill <erik.ekberg@outlook.com>
2020-06-20 12:37:37 -0700

    Remove files

--- services/client/client/src/App.vue
+++ services/client/client/src/App.vue
@@ -1,15 +1,7 @@
 <script>
-import { TabAggregate } from './utilities/TabAggregate';
 // import { publish, subscribe } from './utilities/cloudevent';
 
 export default {
-	created() {
-		const tab = new TabAggregate({
-			tableNumber: '12',
-			waiter: 'William Tell',
-		});
-		console.log(tab.id);
-	},
 };
 </script>
 
--- services/client/client/src/utilities/TabAggregate.js
+++
@@ -1,7 +0,0 @@
-export class TabAggregate {
-	constructor({ tableNumber, waiter }) {
-		this.id = Math.floor(Math.random() * 1000000);
-		this.tableNumber = tableNumber;
-		this.waiter = waiter;
-	}
-}
