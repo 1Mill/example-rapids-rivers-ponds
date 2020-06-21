ea0e85108a4782bb627d0c6e0a377415a6664678
1Mill <erik.ekberg@outlook.com>
2020-06-21 12:45:18 -0700

    List menu items

--- services/menu/index.js
+++ services/menu/index.js
@@ -1,14 +1,15 @@
 const { KAFKA_EVENTTYPE, subscribe } = require('@1mill/cloudevents');
+const { MENU_ITEMS } = require('./lib/menu-items');
 
 subscribe({
 	handler: async ({ isEnriched }) => {
 		if (isEnriched) { return; }
-		return 'testing';
+		return MENU_ITEMS;
 	},
 	id: 'services.menu',
 	publishEventType: KAFKA_EVENTTYPE,
 	publishTo: [ process.env.RAPIDS_URL ],
 	subscribeEventType: KAFKA_EVENTTYPE,
 	subscribeTo: [ process.env.RAPIDS_URL ],
-	types: [ 'add-menu-item.2020-21-06' ],
+	types: [ 'list-menu-items.2020-21-06' ],
 });
---
+++ services/menu/lib/menu-items.js
@@ -0,0 +1,20 @@
+const MENU_ITEMS = [
+	{
+		name: 'old-fashioned',
+		priceUSD: 6,
+	},
+	{
+		name: 'soda',
+		priceUSD: 3,
+	},
+	{
+		name: 'spicey-pickles',
+		priceUSD: 4,
+	},
+	{
+		name: 'tots',
+		priceUSD: 7,
+	},
+];
+
+module.exports = { MENU_ITEMS };
