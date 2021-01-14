15190a27558ed0de7ba6d89dbd41018f57d36067
1Mill <erik.ekberg@outlook.com>
2021-01-13 21:08:25 -0800

    Compile lambdas

---
+++ scripts/lambda/compile.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CONFIG_FILENAME=local.config
+DIRECTORIES=$(
+	find . \
+	-name "$CONFIG_FILENAME" \
+	-not -path "*node_modules*" \
+	-not -path "*nuxt*" \
+	-printf "%h\n"
+)
+
+for dir in $DIRECTORIES
+do
+	# * Reset / configure vars for lambda functions
+	AWS_ENDPOINT=http://localhost:4566/
+	FUNCTIONNAME=
+	HANDLER=
+	HOST_ABSOLUTEPATH=/$(cd -- $directory && pwd)
+	RUNTIME=
+	TYPE=
+
+	# * Import configuration vars for lambda function
+	source $dir/$CONFIG_FILENAME
+
+	# * Skip if config type is not lambda
+	if [[ "$TYPE" != "lambda" ]]
+	then
+		continue
+	fi
+
+	# * Compile lambda
+	DIR=$(pwd)/$dir docker-compose -f $(dirname $BASH_SOURCE)/compile.docker-compose.yml up $RUNTIME
+done
