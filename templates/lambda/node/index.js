efb8783fbc906e6a735161a15bbb355e5d498476
1Mill <erik.ekberg@outlook.com>
2021-01-16 11:10:46 -0800

    Update template

--- templates/lambda/node/index.js
+++ templates/lambda/node/index.js
@@ -1,8 +1,27 @@
-const { v4: { createEventStream } } = require('@1mill/cloudevents')
+const { v4: { createCloudevent, createEventStream } } = require('@1mill/cloudevents')
 
+const rapids = createEventStream({
+	id: process.env.CLOUDEVENT_ID,
+	mechanism: process.env.CLOUDEVENT_MECHANISM,
+	password: process.env.CLOUDEVENT_PASSWORD,
+	urls: process.env.CLOUDEVENT_URLS,
+	username: process.env.CLOUDEVENT_USERNAME,
+
+})
 const perform = async ({ cloudevent }) => {
 	const { message } = JSON.parse(cloudevent.data || JSON.stringify({ message: 'Hello world!' }))
-	console.log(message)
+
+	const enrichment = message.split('').reverse().join('')
+
+	rapids.emit({
+		cloudevent: createCloudevent({
+			...cloudevent,
+			data: JSON.stringify(enrichment),
+			datacontenttype: 'application/json',
+			source: process.env.CLOUDEVENT_ID,
+			type: 'todo.0.e',
+		})
+	})
 }
 
 const lambda = createEventStream({ protocol: 'lambda' })
